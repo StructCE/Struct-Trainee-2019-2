6c743fb2ebbcd96b2d9ae841d9ce6a2aecefe1bc
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-11-13 21:32:28 -0300

    Aula 13/11

--- Aulas/AulaMVC/untitled/app/controllers/task_controller.rb
+++ Aulas/AulaMVC/untitled/app/controllers/task_controller.rb
@@ -5,30 +5,44 @@ class TaskController < ApplicationController
 
     def create
         # Trocar por um método privado do controller (mostrado em aula)
-        @task = Task.new(:title => params[:task][:title],
-                         :description => params[:task][:description],
-                         :date_task => params[:task][:date_task])
+        @task = Task.new(task_params)
         # Melhorar condição para salvar quando terminar o show do usuário
-        @task.save
+        if @task.save
+            redirect_to task_index_path
+        else
+            render 'new'
+        end
+
+
     end
 
     def index
-
+        @tasks = Task.all
     end
 
     def show
-
+        @task = Task.find(params[:id])
     end
 
     def edit
-
+        @task = Task.find(params[:id])
     end
 
     def update
-
+        @task = Task.find(params[:id])
+        @task.update(task_params)
+        redirect_to task_index_path
     end
 
     def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
 
+        redirect_to task_index_path
+    end
+
+    private
+    def task_params
+        params.require(:task).permit(:title, :description, :date_task)
     end
 end
