3a6225bc46ac7927a9cd4e239e2d355a387ea042
Otho Teixeira <otho.tk@hotmail.com>
2019-11-19 14:15:16 -0300

    DOJO_19_11_2019

---
+++ Aulas/DOJO_trainee_2019_2/app/controllers/user_controller_controller.rb
@@ -0,0 +1,47 @@
+class UserControllerController < ApplicationController
+  def new
+    @user = User.new  
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :cpf, :dn, :email, :matricula)
+    end
+end
---
+++ Aulas/DOJO_trainee_2019_2/especificacao_proj_final.md
@@ -0,0 +1,45 @@
+# Projeto final
+
+## Moodle
+
+1. Usuarios
+	- CRUD
+	- Alunos(login - sorcery)
+	- Professores
+
+2. Páginas de disciplinas inscritas
+
+3. Sistema de busca de disciplinas existentes(básica)
+
+4. Tarefas
+	- CRUD
+	-	Nota
+	- Prazo
+	- Anexar arquivo
+
+5. Comentários
+	- CRUD
+	- Edição somente na página do comentário da postagem.
+	- Título
+	- Descrição
+	- Data 
+
+6. Postagens
+	- Texto
+	- Imagem
+	- Link pra vídeo
+	- Arquivos
+	- Vários Comentários
+	- Autoria
+	- Datas
+
+7. Disciplinas
+	- CRUD
+		-	Sistema de inscrição
+	- Chave de entrada
+	- Várias Postagens
+	- Vários professores
+	- Vários alunos
+	- Várias tarefas
+
+Linkar usuário a postagens
