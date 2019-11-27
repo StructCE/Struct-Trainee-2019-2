df06fe4d3d20db9464d97a37449cc0cebdfdec69
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-11-27 13:04:53 -0300

    Figaro

--- Aulas/DOJO_trainee_2019_2/db/schema.rb
+++ Aulas/DOJO_trainee_2019_2/db/schema.rb
@@ -12,10 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20191119160702) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "name"
     t.string "cpf"
     t.date "dn"
